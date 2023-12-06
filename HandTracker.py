bb76b43ec9320276fa83af293dfcfbc7f0b5b283
Lucas Fernandes <118088988+lucasfernandesdeveloper@users.noreply.github.com>
2023-12-05 22:02:16 -0300

    Update HandTracker.py

--- HandTracker.py
+++ HandTracker.py
@@ -27,8 +27,26 @@ while True:
             mp_drawing.draw_landmarks(
                 image,
                 hand_landmarks, mphands.HAND_CONNECTIONS)
-    cv2.imshow('HandTracker', image)
+            # Inside the loop after drawing landmarks
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
 
+                    # Extract the x-coordinate of the middle finger tip (Landmark 8)
+                    x_middle_finger = hand_landmarks.landmark[mp.solutions.hands.HandLandmark.MIDDLE_FINGER_TIP].x
+                    y_middle_finger = hand_landmarks.landmark[mp.solutions.hands.HandLandmark.MIDDLE_FINGER_TIP].y
+
+                    # Get the width of the image
+                    image_width = image.shape[1]
+
+                    percentagex = int(x_middle_finger*100)
+
+                    # Check if the middle finger is on the left half of the image
+                    if (x_middle_finger < 0.5) & (y_middle_finger < 0.5):
+                        print("Esquerda: ", percentagex, "%")
+                    else:
+                        print("Direita: ", percentagex, "%")
+
+                cv2.imshow('HandTracker', image)
     # Stop button
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
